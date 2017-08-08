19348ad4aa57a4cf43231fc51443d84c37174c5f
vangheem <vangheem@gmail.com>
2017-08-08 15:32:02 -0500

    Preparing release 1.0.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,4 +1,4 @@
-1.0.1 (unreleased)
+1.0.1 (2017-08-08)
 ------------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except IOError:
 
 setup(
     name='guillotina_prometheus',
-    version='1.0.1.dev0',
+    version='1.0.1',
     description='prometheus integration into guillotina',
     long_description=README,
     install_requires=[
