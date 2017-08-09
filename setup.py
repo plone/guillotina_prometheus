ae0f0d41b9ff8660c140bb703a95095bad4003eb
vangheem <vangheem@gmail.com>
2017-08-09 09:40:39 -0500

    Back to development: 1.0.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except IOError:
 
 setup(
     name='guillotina_prometheus',
-    version='1.0.5',
+    version='1.0.6.dev0',
     description='prometheus integration into guillotina',
     long_description=README,
     install_requires=[
